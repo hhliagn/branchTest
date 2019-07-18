7ba47645f32e1b685556a18f36c64038e754e652
hhliagn <hhliagn@gmail.com>
2019-07-18 11:19:13 +0800

    dev111

--- src/main/java/cc.java
+++ src/main/java/cc.java
@@ -1,6 +1,10 @@
 public class cc {
 
     public static void main(String[] args) {
-        System.out.println("cc");
+        System.out.println("ee");
+    }
+
+    public void test(){
+        System.out.println("dd");
     }
 }
